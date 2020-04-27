c936fc736c3e2e8f573b0687a43edd7461c0a43e
LuGew <lugew@outlook.com>
2020-04-27 22:04:56 +0800

    init

---
+++ src/main/java/com/lugew/study/softwaredesigner/SoftwareDesignerApplication.java
@@ -0,0 +1,13 @@
+package com.lugew.study.softwaredesigner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareDesignerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftwareDesignerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lugew/study/softwaredesigner/SoftwaredesignerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lugew.study.softwaredesigner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwaredesignerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
