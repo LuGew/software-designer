09f0b9b7cc8f4df7df86428c55691cff7a0ceb5e
LuGew <lugew@outlook.com>
2020-05-09 22:52:15 +0800

    origin code

--- src/main/java/com/lugew/study/softwaredesigner/network/number/strategy/SingleIntegerStrategy.java
+++ src/main/java/com/lugew/study/softwaredesigner/network/number/strategy/SingleIntegerStrategy.java
@@ -18,8 +18,23 @@ public class SingleIntegerStrategy extends SingleStrategy<Integer> {
         return instance;
     }
 
+
     @Override
     public String binary(Integer integer) {
+        /*chars[0] = integer < 0 ? ONE : ZERO;
+
+        int index = 31;
+        while (integer != 0) {
+            chars[index] = map.get(integer % 2);
+            integer /= 2;
+            index--;
+        }
+        zeroPadding(index);
+        return Arrays.toString(chars);*/
+        return "";
+    }
+
+    private char[] trueFormChars(Integer integer) {
         chars[0] = integer < 0 ? ONE : ZERO;
 
         int index = 31;
@@ -29,6 +44,35 @@ public class SingleIntegerStrategy extends SingleStrategy<Integer> {
             index--;
         }
         zeroPadding(index);
+        return chars;
+    }
+
+    @Override
+    public String trueForm(Integer integer) {
+        return Arrays.toString(trueFormChars(integer));
+    }
+
+    @Override
+    public String onesComplement(Integer integer) {
+        char[] chars = trueFormChars(integer);
+        for (int i = 1, length = chars.length; i < length; i++) {
+            chars[i] = (chars[i] == ZERO ? ONE : ZERO);
+        }
         return Arrays.toString(chars);
+    }
+
+    @Override
+    public String twosComplement(Integer integer) {
+        return null;
+    }
+
+    @Override
+    public String offsetBinary(Integer integer) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        SingleIntegerStrategy singleIntegerStrategy = new SingleIntegerStrategy();
+        System.out.println(singleIntegerStrategy.trueForm(-1) + singleIntegerStrategy.onesComplement(-1));
     }
 }
--- src/main/java/com/lugew/study/softwaredesigner/network/number/strategy/Strategy.java
+++ src/main/java/com/lugew/study/softwaredesigner/network/number/strategy/Strategy.java
@@ -1,9 +1,47 @@
 package com.lugew.study.softwaredesigner.network.number.strategy;
 
 /**
- * @author XINWEN95
+ * @author LuGew
  * @since 2020/4/28 23:37
  **/
 public interface Strategy<T extends Number> {
+    /**
+     * 二进制
+     *
+     * @param t 数
+     * @return 字符表示
+     */
     String binary(T t);
+
+    /**
+     * 原码
+     *
+     * @param t 数
+     * @return 字符表示
+     */
+    String trueForm(T t);
+
+    /**
+     * 反码
+     *
+     * @param t 数
+     * @return 字符表示
+     */
+    String onesComplement(T t);
+
+    /**
+     * 补码
+     *
+     * @param t 数
+     * @return 字符表示
+     */
+    String twosComplement(T t);
+
+    /**
+     * 移码
+     *
+     * @param t 数
+     * @return 字符表示
+     */
+    String offsetBinary(T t);
 }
