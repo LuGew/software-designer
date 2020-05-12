0e60fe9f9f60bf507c5ccc14cb79b748ce621e17
LuGew <lugew@outlook.com>
2020-05-12 08:27:37 +0800

    origin code

--- src/test/java/com/lugew/study/softwaredesigner/network/number/strategy/SingleIntegerStrategySpecification.java
+++ src/test/java/com/lugew/study/softwaredesigner/network/number/strategy/SingleIntegerStrategySpecification.java
@@ -2,7 +2,9 @@ package com.lugew.study.softwaredesigner.network.number.strategy;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -10,12 +12,20 @@ import static org.junit.jupiter.api.Assertions.*;
  * @since 2020/5/12
  */
 class SingleIntegerStrategySpecification {
+    SingleIntegerStrategy singleIntegerStrategy;
 
     @BeforeEach
     void setUp() {
+        singleIntegerStrategy = SingleIntegerStrategy.getInstance();
     }
 
     @AfterEach
     void tearDown() {
+    }
+
+    @Test
+    void givenTrueFormWhenInput1ThenOutput00000000000000000000000000000001() {
+        assertThat(singleIntegerStrategy.trueForm(1))
+                .isEqualTo("00000000000000000000000000000001".toCharArray());
     }
 }
