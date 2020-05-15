e5264fc2e2fbe77d4182bf66afd135f918a18594
LuGew <lugew@outlook.com>
2020-05-15 13:54:28 +0800

    add SingleIntegerStrategySpecification

--- src/test/java/com/lugew/study/softwaredesigner/network/number/strategy/SingleIntegerStrategySpecification.java
+++ src/test/java/com/lugew/study/softwaredesigner/network/number/strategy/SingleIntegerStrategySpecification.java
@@ -28,4 +28,40 @@ class SingleIntegerStrategySpecification {
         assertThat(singleIntegerStrategy.trueForm(1))
                 .isEqualTo("00000000000000000000000000000001".toCharArray());
     }
+
+    @Test
+    void givenTrueFormWhenInputNegative1ThenOutput10000000000000000000000000000001() {
+        assertThat(singleIntegerStrategy.trueForm(-1))
+                .isEqualTo("10000000000000000000000000000001".toCharArray());
+    }
+
+    @Test
+    void givenTrueFormWhenInputIntegerMinValueThenOutput10000000000000000000000000000000() {
+        assertThat(singleIntegerStrategy.trueForm(Integer.MIN_VALUE))
+                .isEqualTo("10000000000000000000000000000000".toCharArray());
+    }
+
+    @Test
+    void givenTrueFormWhenInputIntegerMaxValueThenOutput01111111111111111111111111111111() {
+        assertThat(singleIntegerStrategy.trueForm(Integer.MAX_VALUE))
+                .isEqualTo("01111111111111111111111111111111".toCharArray());
+    }
+
+    @Test
+    void givenTrueFormWhenInput6ThenOutput00000000000000000000000000000110() {
+        assertThat(singleIntegerStrategy.trueForm(6))
+                .isEqualTo("00000000000000000000000000000110".toCharArray());
+    }
+
+    @Test
+    void givenTrueFormWhenInputNegative6ThenOutput10000000000000000000000000000110() {
+        assertThat(singleIntegerStrategy.trueForm(-6))
+                .isEqualTo("10000000000000000000000000000110".toCharArray());
+    }
+
+    @Test
+    void givenTrueFormWhenInput0ThenOutput00000000000000000000000000000000() {
+        assertThat(singleIntegerStrategy.trueForm(0))
+                .isEqualTo("00000000000000000000000000000000".toCharArray());
+    }
 }
